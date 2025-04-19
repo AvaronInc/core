a7410d9a919d87d34f761df3c578bfc3ee936e09
Jeremy Bobbin <jer@jer.cx>
2025-04-18 22:20:04 -0700

    test Route() & sorting

--- net/linux/lib_test.go
+++ net/linux/lib_test.go
@@ -3,6 +3,7 @@ package netlink
 import (
 	"fmt"
 	"os"
+	"sort"
 	"testing"
 	//"context"
 )
@@ -12,5 +13,28 @@ func TestList(t *testing.T) {
 	fmt.Fprintf(os.Stdout, "err: %+v\n", err)
 	for _, i := range list {
 		fmt.Fprintf(os.Stdout, "interface: %+v\n", i)
+		for _, a := range i.AddrInfo {
+			fmt.Fprintf(os.Stdout, "addr info: %+v\n", a)
+		}
+		fmt.Fprintf(os.Stdout, "sorted\n\n")
+		sort.Sort(AddressMask(i.AddrInfo))
+	}
+
+	routes, err := Routes(t.Context())
+	fmt.Fprintf(os.Stdout, "err: %+v\n", err)
+	for _, r := range routes {
+		fmt.Fprintf(os.Stdout, "%+v\n", r)
+	}
+
+	fmt.Fprintf(os.Stdout, "sorted\n")
+	sort.Sort(RouteMask(routes))
+	for _, r := range routes {
+		fmt.Fprintf(os.Stdout, "%+v\n", r)
+	}
+
+	metrics, err := Metrics(t.Context())
+	fmt.Fprintf(os.Stdout, "err: %+v\n", err)
+	for _, m := range metrics {
+		fmt.Fprintf(os.Stdout, "%+v\n", m)
 	}
 }
