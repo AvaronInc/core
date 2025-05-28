d3504f9671eddcb5d4d1a1a95aca09ecab80a35d
Jeremy Bobbin <jer@jer.cx>
2025-05-27 22:20:57 -0700

    service description panel

--- public/services/index.jsx
+++ public/services/index.jsx
@@ -3,21 +3,28 @@ import Frame from '../frame'
 import {size} from '../util'
 import ReactDOM from 'react-dom/client';
 
-function style(s) {
-	switch (s) {
-	case "exited":
-		return "info"
-	case "dead":
-		return "danger"
-	case "running":
-		return "success"
-	default:
-		return "warning"
+function style(active, sub) {
+	let s = ["secondary", "secondary"]
+	switch (active) {
+	case "inactive": return s
+	case "failed": s[0] = "warning"; break
+	default:       s[0] = "info";    break
 	}
+
+	switch (sub) {
+	case "exited":  s[1] = "secondary"; break;
+	case "dead":    s[1] = "warning";   break;
+	case "failed":  s[1] = "danger";    break;
+	case "running": s[1] = "success";   break;
+	default:        s[1] = "danger";    break;
+	}
+
+	return s
 }
 
 const List = () => {
 	const [services, setServices] = useState(null)
+	const [focus, setFocus] = useState(null)
 
 	useEffect(() => {
 		fetch("/api/services")
@@ -26,24 +33,57 @@ const List = () => {
 			.then(setServices)
 	}, [])
 
-	const rows = new Array(size(services))
+	const keys = services ? Object.keys(services) : []
+	keys.sort((a, b) => {
+		const as = services[a].ActiveState, bs = services[b].ActiveState
+		if (as === bs || (as === "active" && bs === "failed") || (as === "failed" && bs === "active")) {
+			// ok
+		} else if (services[a].ActiveState == "inactive") {
+			return 1
+		} else {
+			return -1
+		}
+
+		if (a === b) {
+			return 0
+		} else if (a < b) {
+			return -1
+		} else {
+			return 1
+		}
+
+	})
+	
+	const rows = new Array(keys.length)
 
 	let i = 0
-	for (const key in services) {
+	for (const key of keys) {
 		const s = services[key]
-
+		const styles = style(s.ActiveState, s.SubState)
 		rows[i++] = (
 			<tr
 				key={key}
-				class={false ? "table-active" : ""}
-				onClick={null}
+				class={(key === focus) ? "table-active" : ""}
+				onClick={setFocus.bind(null, key)}
 			>
 				<td>{s.Name.replace(/\.service$/, "")}</td>
 				<td>
-					<span class={"m-1 py-1 px-2 text-center rounded-pill bg-"+style(s.SubState)}>
-						{s.SubState}
-					</span>
+					<strong class={"m-1 py-1 px-2 rounded-pill bg-"+styles[0]}>
+						{s.ActiveState}
+					</strong>
 				</td>
+				<td>
+					<strong class={"m-1 py-1 px-2 rounded-pill bg-"+styles[1]}>
+						{s.SubState}
+					</strong>
+				</td>
+				{focus ? null : (
+					<td>
+						<span class={"m-1 py-1 px-2"}>
+							{s.Description}
+						</span>
+					</td>
+				)}
 			</tr>
 		)
 	}
@@ -51,39 +91,29 @@ const List = () => {
 	return (
 		<Frame>
 			<div class="d-flex flex-row w-100">
-				<div class="card text-bg-dark flex-fill overflow-x-auto me-2 ">
-					<div class="card-body">
-						<table class="table table-dark">
-							<thead>
-								<tr>
-									<th scope="col">Name</th>
-									<th scope="col">State</th>
-								</tr>
-							</thead>
-							<tbody>
-								{rows}
-							</tbody>
-						</table>
+				{services ? (
+					<div class="card text-bg-dark overflow-x-auto me-2 ">
+						<div class="card-body">
+							<table class="table table-dark">
+								<tbody>
+									{rows}
+								</tbody>
+							</table>
+						</div>
 					</div>
-				</div>
-				<div class="card text-bg-dark flex-fill overflow-x-auto me-2 ">
-					<div class="card-header">
-						List
+				) : null}
+				{focus ? (
+					<div class="card text-bg-dark flex-fill overflow-x-auto me-2 ">
+						<div class="card-header">
+							{focus}
+						</div>
+						<div class="card-body">
+							<p>
+								{services[focus].Description}
+							</p>
+						</div>
 					</div>
-					<div class="card-body">
-						<table class="table table-dark">
-							<thead>
-								<tr>
-									<th scope="col">Name</th>
-									<th scope="col">State</th>
-								</tr>
-							</thead>
-							<tbody>
-								{rows}
-							</tbody>
-						</table>
-					</div>
-				</div>
+				) : null}
 			</div>
 		</Frame>
 	)
